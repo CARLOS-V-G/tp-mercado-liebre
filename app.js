798db3a0c65db9be95ebb3709a98e1fe2e069524
CARLOS-V-G <cv5944341@gmail.com>
2023-01-09 13:59:11 -0300

    agregando login y register

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html'))); 
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
